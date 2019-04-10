1fd317feb577d672520e18523759726726ac1280
Madeline Briere <mfb33@duke.edu>
2019-04-10 17:05:23 -0400

    Create guitar.m

---
+++ guitar.m
@@ -0,0 +1,31 @@
+function output = guitar(f, l)
+Fs = 44100;
+N = Fs/f;
+
+fr = zeros(1, N/2);
+fl = zeros(1, N/2);
+
+% Piecewise waveform.
+pluck_point = N/10;
+sample_point = N/4;
+start = [linspace(0, 1, pluck_point) linspace(1, 0, N/2-pluck_point)];
+fr = start;
+fl = start;
+
+record_length = l * Fs;
+output = zeros(1, record_length);
+for i = 1:record_length
+    output(1, i) = fr(1, sample_point) + fl(1, sample_point);
+    fl_temp = fl;
+    fr_temp = fr;
+    fl(1, N/2) = -fr_temp(1, N/2);
+    fr(1, 1) = -fl_temp(1, 1);
+    fr(1, 2:N/2-1) = fr_temp(1, 1:N/2-2);
+    fl(1, 1:N/2-1) = fl_temp(1, 2:N/2);
+    % Filter.
+    a = .02;
+    y = .99;
+    fr(1, N/2) = y.*(-a.*fl_temp(1, N/2) + (1-a).*fr_temp(1, N/2-1));
+end 
+soundsc(output, Fs);
+end
